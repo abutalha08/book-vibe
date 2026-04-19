77b4f116c1bd56ea0688d6d15f9c363cf1039df0
abutalha08 <taufique752@gmail.com>
2026-04-19 21:24:30 +0600

    path setup done using react router

---
+++ src/Pages/Books/Books.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Books = () => {
+    return (
+        <div>
+            All books here
+        </div>
+    );
+};
+
+export default Books;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layout/MainLayout";
+import Homepage from "../Pages/Homepage/Homepage";
+import Books from "../Pages/Books/Books";
+import BookDetails from "../Pages/BookDetails/BookDetails";
+
+export const router = createBrowserRouter ([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        children:[
+            {
+                index: true,
+                element : <Homepage></Homepage>
+            },
+            {
+                path: "/books",
+                element: <Books></Books>
+            },
+            {
+                path: "/bookDetails",
+                element: <BookDetails></BookDetails>
+            }
+        ]
+    }
+])
